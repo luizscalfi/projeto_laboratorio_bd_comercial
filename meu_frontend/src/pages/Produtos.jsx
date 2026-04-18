dd5864369530616dd80926ecde8b265cf071d989
luizscalfi <luizcarlostheodoro@hotmail.com>
2026-04-18 00:08:50 -0300

    fix: renomeando css para forçar o git a reconhecer no linux

--- meu_frontend/src/pages/Produtos.jsx
+++ meu_frontend/src/pages/Produtos.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { Trash2, AlertCircle } from 'lucide-react';
-import './styles/produto.css';
+import './styles/estilo_produto.css';
 
 function Produtos() {
   const [produtos, setProdutos] = useState([]);
